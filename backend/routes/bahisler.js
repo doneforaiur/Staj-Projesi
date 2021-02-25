6fa7231706e71b4f3d789137263779143c79cd7e
Hamsi <mirza.a.96@hotmail.com>
2021-02-25 17:50:04 +0300

    heroku

--- backend/routes/bahisler.js
+++ backend/routes/bahisler.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 let Bahis = require('../models/bahis.model');
 
 router.route('/').get((req, res) => {
+  console.log("girdi", req)
   Bahis.find()
     .then(bahisler => res.json(bahisler))
     .catch(err => res.status(400).json('Hata; ' + err));
